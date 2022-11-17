cac32f59957c982c78ffe4ddf26df3bbd4c5443a
dia-me-tr <diana.tryhub@gmail.com>
2022-11-17 13:47:40 +0200

    lab2 added

---
+++ AI/lab2/gaus_fm.py
@@ -0,0 +1,146 @@
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+my = ctrl.Antecedent(np.arange(0, 1.01, 0.01), "my")
+mx = ctrl.Antecedent(np.arange(0, 1.01, 0.01), "mx")
+mf = ctrl.Consequent(np.arange(0.8, 1.9, 0.01), "mf")
+
+
+mx["mx1"] = fuzz.gaussmf(mx.universe, 0, 0.075)
+mx["mx2"] = fuzz.gaussmf(mx.universe, 0.2, 0.075)
+mx["mx3"] = fuzz.gaussmf(mx.universe, 0.4, 0.075)
+mx["mx4"] = fuzz.gaussmf(mx.universe, 0.6, 0.075)
+mx["mx5"] = fuzz.gaussmf(mx.universe, 0.8, 0.075)
+mx["mx6"] = fuzz.gaussmf(mx.universe, 1.0, 0.075)
+
+my["my1"] = fuzz.gaussmf(my.universe, 0, 0.075)
+my["my2"] = fuzz.gaussmf(my.universe, 0.2, 0.075)
+my["my3"] = fuzz.gaussmf(my.universe, 0.4, 0.075)
+my["my4"] = fuzz.gaussmf(my.universe, 0.6, 0.075)
+my["my5"] = fuzz.gaussmf(my.universe, 0.8, 0.075)
+my["my6"] = fuzz.gaussmf(my.universe, 1.0, 0.075)
+
+for i in range(9):
+    mf[f"mf{i+1}"] = fuzz.gaussmf(mf.universe, 0.9 + 0.11 * i, 0.11)
+    # print([0.9 + (0.11 * (i-0.5)), 0.9 + 0.11 * i, 0.9 + (0.11 * (i+0.5))])
+
+rule_mf1 = ctrl.Rule(antecedent=(mx["mx1"] & my["my5"] |
+                                 mx["mx1"] & my["my6"]),
+                                 consequent=mf["mf1"], label = 'rule mf1')
+rule_mf2 = ctrl.Rule(antecedent=(mx["mx1"] & my["my1"]|
+                                 mx["mx1"] & my["my2"]|
+                                 mx["mx1"] & my["my3"]|
+                                 mx["mx1"] & my["my4"]),
+                     consequent=mf["mf2"], label= "rule mf2")
+
+rule_mf3 = ctrl.Rule(antecedent=(mx["mx2"] & my["my4"]|
+                                 mx["mx2"] & my["my5"]|
+                                 mx["mx2"] & my["my6"]),
+                     consequent = mf["mf3"], label = "rule mf3")
+
+rule_mf4 = ctrl.Rule(antecedent=(mx["mx3"] & my["my6"] |
+                                 mx["mx2"] & my["my1"] |
+                                 mx["mx2"] & my["my2"] |
+                                 mx["mx2"] & my["my3"]),
+                     consequent=mf["mf4"], label="rule mf4")
+
+rule_mf5 = ctrl.Rule(antecedent=(mx["mx3"] & my["my1"] |
+                                 mx["mx3"] & my["my2"] |
+                                 mx["mx3"] & my["my3"] |
+                                 mx["mx3"] & my["my4"] |
+                                 mx["mx3"] & my["my5"]),
+                     consequent=mf["mf5"], label= "rule mf5")
+
+rule_mf6 = ctrl.Rule(antecedent=(mx["mx4"] & my["my5"] |
+                                 mx["mx4"] & my["my6"]),
+                     consequent=mf["mf6"], label= "rule mf6")
+
+rule_mf7 = ctrl.Rule(antecedent=(mx["mx4"] & my["my1"] |
+                                 mx["mx4"] & my["my2"] |
+                                 mx["mx4"] & my["my3"] |
+                                 mx["mx4"] & my["my4"] |
+                                 mx["mx5"] & my["my6"]),
+                     consequent=mf["mf7"], label= "rule mf7")
+
+rule_mf8 = ctrl.Rule(antecedent=(mx["mx5"] & my["my1"] |
+                                 mx["mx5"] & my["my2"] |
+                                 mx["mx5"] & my["my3"] |
+                                 mx["mx5"] & my["my4"] |
+                                 mx["mx5"] & my["my5"] |
+                                 mx["mx6"] & my["my6"]),
+                     consequent=mf["mf8"], label= "rule mf8")
+
+rule_mf9 = ctrl.Rule(antecedent=(mx["mx6"] & my["my1"] |
+                                 mx["mx6"] & my["my2"] |
+                                 mx["mx6"] & my["my3"] |
+                                 mx["mx6"] & my["my4"] |
+                                 mx["mx6"] & my["my5"]),
+                     consequent=mf["mf9"], label= "rule mf9")
+
+system = ctrl.ControlSystem(rules=[rule_mf1,rule_mf2,rule_mf3,rule_mf4,rule_mf5, rule_mf6, rule_mf7, rule_mf8, rule_mf9])
+
+my.view()
+time.sleep(5)
+mx.view()
+time.sleep(5)
+mf.view()
+time.sleep(5)
+plt.show()
+
+sim = ctrl.ControlSystemSimulation(system)
+upsampled = [0, 0.2, 0.4, 0.6, 0.8, 1]
+
+x, y = np.meshgrid(upsampled, upsampled)
+z = np.zeros_like(x)
+
+
+for i in range(len(upsampled)):
+    for j in range(len(upsampled)):
+        # print(x[i, j], y[i, j], end=' ')
+        sim.input['mx'] = x[i, j]
+        sim.input['my'] = y[i, j]
+        sim.compute()
+        z[i, j] = sim.output['mf']
+        # print(z[i, j])
+
+
+fig = plt.figure(figsize=(9, 9))
+ax = fig.add_subplot(111, projection='3d')
+
+surf = ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap='viridis', linewidth=0.4, antialiased=True)
+
+cset = ax.contourf(x, y, z, zdir='z', offset=-2.5, cmap='viridis', alpha=0.5)
+cset = ax.contourf(x, y, z, zdir='x', offset=3, cmap='viridis', alpha=0.5)
+cset = ax.contourf(x, y, z, zdir='y', offset=3, cmap='viridis', alpha=0.5)
+
+ax.view_init(30, 200)
+
+
+plt.show()
+x_y = np.arange(0, 1.2, 0.2)
+z_2d = np.zeros_like(x_y)
+x_g = np.arange(0, 1, 0.05)
+
+z_r = np.sin(x_y) + np.cos(x_y / 2)
+z_g = np.sin(x_g) + np.cos(x_g / 2)
+
+for i in range(len(x_y)):
+    sim.input['mx'] = x_y[i]
+    sim.input['my'] = x_y[i]
+    sim.compute()
+    z_2d[i] = sim.output['mf']
+
+
+e = abs(z_r - z) / z_r
+print(f"Error = {round(np.mean(e) * 100, 3)} %")
+
+plt.plot(x_y, z_2d)
+plt.plot(x_g, z_g)
+plt.title("Порівняння моделі побудованою за доп. gaussmf та оригінальної")
+plt.xlabel(f"Error = {round(np.mean(e) * 100, 3)} %")
+plt.ylabel("mf")
+plt.show()
+
